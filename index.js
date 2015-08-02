fa6f27dabf3c50895e9f7a0703e40962b0e1056c
Dale Bustad <dale@divmain.com>
2015-08-02 00:25:40 -0700

    Update transform to accomodate more explicit transform API changes.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = function (opts={}) {
   var isJsonFile = opts.filter || /\.json$/;
 
   return function (override, transform, control) {
-    transform("readSource", function (source) {
-      const asset = this.args[0];
+    transform("readSource", function (source, args) {
+      const asset = args[0];
       if (isJsonFile.test(asset.path)) {
         source = `module.exports = ${source};`;
       }
