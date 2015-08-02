dba9272cbb87cd7c13f9eaa3969649d44aa26511
Dale Bustad <dale@divmain.com>
2015-08-02 16:07:46 -0700

    First pass at plugin.
    - templates transformed into JS
    - JS transformed into module
    - handlebars runtime loads correctly

---
+++ index.js
@@ -0,0 +1,78 @@
+// TODO: Add proper sourcemap support.
+
+import * as path from "path";
+
+import * as Handlebars from "handlebars";
+
+import resolveAsset from "interlock/lib/compile/modules/resolve";
+import { readSource } from "interlock/lib/compile/modules/load-ast";
+import compileModules from "interlock/lib/compile/modules/compile";
+
+function DependenciesVisitor() {
+  this.partials = [];
+  this.helpers = [];
+  Handlebars.Visitor.apply(this, arguments);
+}
+
+DependenciesVisitor.prototype = Object.create(Handlebars.Visitor.prototype);
+
+Object.assign(DependenciesVisitor.prototype, {
+  PartialStatement: function(partial) {
+    this.partials.push({request: partial.name.original});
+    Handlebars.Visitor.prototype.PartialStatement.call(this, partial);
+  },
+
+  sexpr: function(sexpr) {
+    var id = sexpr.id;
+    if (id.isSimple) {
+      this.helpers.push(id.original);
+    }
+    Handlebars.Visitor.prototype.sexpr.call(this, sexpr);
+  }
+});
+
+
+function getHbsRequires (ast, knownHelpers, extension) {
+  var requires = "var Handlebars = require('handlebars/runtime');\n";
+
+  var depsVisitor = new DependenciesVisitor();
+  depsVisitor.accept(ast);
+
+  depsVisitor.partials.forEach(partial => {
+    const partialName = partial.name.name.endsWith("." + extension) ?
+      partial.name.name :
+      partial.name.name + "." + extension;
+    requires = requires + `Handlebars.registerPartial("${partialName}", require(${partialName});\n`;
+  });
+
+  depsVisitor.helpers.forEach(helper => {
+    if (!knownHelpers[helper]) {
+      helper = helper.endsWith("." + extension) ?
+        helper :
+        helper + "." + extension;
+      requires = requires + `Handlebars.registerHelper("${helper}", require(${helper}));\n`;
+    }
+  });
+
+  return requires;
+}
+
+module.exports = function (opts={}) {
+  const extension = opts.extension || "hbs";
+  const isHbsFile = new RegExp(`\\.(${extension})$`);
+  const knownHelpers = (opts.knownHelpers || []).reduce((obj, entry) => {
+    obj[entry] = true;
+    return obj;
+  }, {});
+
+  return function (override, transform, control) {
+    transform("readSource", function (source, [asset]) {
+      if (isHbsFile.test(asset.path)) {
+        const ast = Handlebars.parse(source);
+        source = getHbsRequires(ast, knownHelpers, extension) +
+          `module.exports = Handlebars.template(${Handlebars.precompile(ast)});\n`;
+      }
+      return source;
+    });
+  };
+};
