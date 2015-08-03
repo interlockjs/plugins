0b972773b3cb1600c0c8e225851c72ad9c33691e
Dale Bustad <dale@divmain.com>
2015-08-02 21:40:03 -0700

    Working partial resolution and bundling.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // TODO: Add proper sourcemap support.
 
+import path from "path";
+
 import * as Handlebars from "handlebars";
 
-function DependenciesVisitor () {
+import resolve from "interlock/lib/resolve";
+
+function DependenciesVisitor (context) {
+  this.context = context;
   this.partials = [];
   this.helpers = [];
   Handlebars.Visitor.apply(this, arguments);
@@ -12,7 +17,12 @@ DependenciesVisitor.prototype = Object.create(Handlebars.Visitor.prototype);
 
 Object.assign(DependenciesVisitor.prototype, {
   PartialStatement: function (partial) {
-    this.partials.push({request: partial.name.original});
+    const partialAsset = this.resolve(partial.name.original);
+    const uniqueName = `${partialAsset.ns}:${partialAsset.nsPath}`;
+    const relPath = path.relative(this.context.contextPath + "/", partialAsset.path);
+    partial.name.original = partial.name.parts[0] = uniqueName;
+    this.partials.push({ uniqueName, relPath });
+
     Handlebars.Visitor.prototype.PartialStatement.call(this, partial);
   },
 
@@ -22,22 +32,36 @@ Object.assign(DependenciesVisitor.prototype, {
       this.helpers.push(id.original);
     }
     Handlebars.Visitor.prototype.sexpr.call(this, sexpr);
+  },
+
+  resolve: function (name) {
+    const resolved = resolve(
+      name,
+      this.context.contextPath,
+      this.context.ns,
+      this.context.nsRoot,
+      this.context.extensions,
+      this.context.searchPaths
+    );
+    if (!resolved) {
+      throw new Error(
+        `Unable to resolve Handlebars asset ${name} from ${this.context.contextPath}`
+      );
+    }
+    return resolved;
   }
 });
 
 
-function getHbsRequires (ast, knownHelpers, extension) {
+function getHbsRequires (ast, knownHelpers, extension, context) {
   var requires = "var Handlebars = require('handlebars/runtime');\n";
 
-  var depsVisitor = new DependenciesVisitor();
+  var depsVisitor = new DependenciesVisitor(context);
   depsVisitor.accept(ast);
 
   depsVisitor.partials.forEach(partial => {
-    const partialRequire = partial.name.name.endsWith("." + extension) ?
-      partial.name.name :
-      partial.name.name + "." + extension;
     requires = requires +
-      `Handlebars.registerPartial("${partial.name.name}", require(${partialRequire});\n`;
+      `Handlebars.registerPartial("${partial.uniqueName}", require("${partial.relPath}"));\n`;
   });
 
   depsVisitor.helpers.forEach(helper => {
@@ -54,6 +78,7 @@ function getHbsRequires (ast, knownHelpers, extension) {
 
 module.exports = function (opts={}) {
   const extension = opts.extension || "hbs";
+  const searchPaths = opts.searchPaths || [];
   const isHbsFile = new RegExp(`\\.(${extension})$`);
   const knownHelpers = (opts.knownHelpers || []).reduce((obj, entry) => {
     obj[entry] = true;
@@ -63,8 +88,15 @@ module.exports = function (opts={}) {
   return function (override, transform, control) {
     transform("readSource", function (source, [asset]) {
       if (isHbsFile.test(asset.path)) {
+        const context = {
+          contextPath: path.dirname(asset.path),
+          ns: asset.ns || this.opts.ns,
+          nsRoot: asset.nsRoot || this.opts.nsRoot,
+          extensions: ["." + extension],
+          searchPaths: searchPaths
+        };
         const ast = Handlebars.parse(source);
-        source = getHbsRequires(ast, knownHelpers, extension) +
+        source = getHbsRequires(ast, knownHelpers, extension, context) +
           `module.exports = Handlebars.template(${Handlebars.precompile(ast)});\n`;
       }
       return source;
