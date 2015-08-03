a2140bb0f93fabc4be674493ebfdf4773fd2c8a5
Dale Bustad <dale@divmain.com>
2015-08-03 14:42:48 -0700

    Link to documentation.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function (opts={}) {
   var isJsonFile = opts.filter || /\.json$/;
 
   return function (override, transform, control) {
+    // https://github.com/interlockjs/interlock/blob/master/docs/extensibility.md#readsource
     transform("readSource", function (source, args) {
       const asset = args[0];
       if (isJsonFile.test(asset.path)) {
