ed0934d3b7ffa0ae368d1378bf4d0df1c4da5583
Dale Bustad <dale@divmain.com>
2015-09-22 01:41:05 -0700

    Move plugin file to src, add wrapper index.js.

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-const UglifyJS = require("uglify-js");
+var useTranspiled = true; // eslint-disable-line no-var
 
-function uglify (ast, options, mangle) {
-  let uAST = UglifyJS.AST_Node.from_mozilla_ast(ast);
-  uAST.figure_out_scope();
-  uAST = uAST.transform(UglifyJS.Compressor(options)); // eslint-disable-line new-cap
-
-  if (mangle) {
-    uAST.figure_out_scope();
-    uAST.compute_char_frequency();
-    uAST.mangle_names();
-  }
-
-  return uAST.to_mozilla_ast();
+try {
+  require.resolve("./lib");
+} catch (e) {
+  useTranspiled = false;
 }
 
-module.exports = function (uglifyOpts, mangle) {
-  uglifyOpts = uglifyOpts || {};
-  return function (override, transform) {
-    transform("constructBundle", function (bundleAst) {
-      return uglify(bundleAst, uglifyOpts, mangle);
-    });
-  };
-};
+if (useTranspiled) {
+  module.exports = require("./lib");
+} else {
+  require("babel/register");
+  module.exports = require("./src");
+}
