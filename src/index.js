39448f0f307a765ca3195f212f81c46e7f4e7336
Dale Bustad <dale@divmain.com>
2015-09-24 20:21:06 -0700

    Bring `readSource` transform up-to-date with Interlock API.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 export default function (opts = {}) {
   const isJsonFile = opts.filter || /\.json$/;
   return (override, transform) => {
-    transform("readSource", function (source, [module]) {
+    transform("readSource", function (module) {
       if (isJsonFile.test(module.path)) {
-        source = `module.exports = ${source};`;
+        module = Object.assign({}, module, {
+          rawSource: `module.exports = ${module.rawSource};`
+        });
       }
-      return source;
+      return module;
     });
   };
 }
