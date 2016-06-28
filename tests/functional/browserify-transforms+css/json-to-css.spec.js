02b467b996c534bb7f5c3cfc49c27d622f978e34
Dale Bustad <dale@divmain.com>
2016-06-27 22:51:32 -0700

    Clean up spec file for `browserify-transforms` + `css`.

--- tests/functional/browserify-transforms+css/json-to-css.spec.js
+++ tests/functional/browserify-transforms+css/json-to-css.spec.js
@@ -14,42 +14,22 @@ const getConfig = opts => ({
 
   entry: opts.entry,
   plugins: [
-  	browserifyTransforms(opts.transforms),
+    browserifyTransforms(opts.transforms),
     css(opts.cssOpts)
   ]
 });
 
-
-function fakeRequire(files, path) {
-  // This is _not_ robust.  But it is simple and it works for
-  // our test input.
-  path = path.replace(/^\.\.?\//, "");
-
-  if (!(path in files)) {
-    const altPath = `${path}.js`;
-    if (altPath in files) {
-      path = altPath;
-    } else {
-      throw new Error(`Cannot find '${path}' in files.`);
-    }
-  }
-  const _module = { exports: {} };
-  const fn = new Function("require", "module", files[path]);
-  fn(dependency => fakeRequire(files, dependency), _module);
-  return _module.exports;
-}
-
-
 describe("interlock-browserify-transforms + interlock-css", () => {
-	describe("transforming json to css to javascript", () => {
+  describe("transforming json to css to javascript", () => {
     const config = getConfig({
       entry: {
         "./a.js": "a.bundle.js"
       },
       transforms: [{
-      	transform: require("./json-to-css/transform-json-to-css"),
-      	filter: /\.styles\.json$/,
-      	moduleType: "css"
+        // eslint-disable-next-line global-require
+        transform: require("./json-to-css/transform-json-to-css"),
+        filter: /\.styles\.json$/,
+        moduleType: "css"
       }],
       cssOpts: {
         mode: "bundle"
@@ -61,7 +41,7 @@ describe("interlock-browserify-transforms + interlock-css", () => {
       return getFilesFromConfig(config).then(_files => files = _files);
     });
 
-    it("does stuff!", () => {
+    it("applies custom transform and behaves otherwise as a normal CSS input", () => {
       const {
         insertScript,
         testResult
@@ -75,5 +55,5 @@ describe("interlock-browserify-transforms + interlock-css", () => {
         expect(cssFile).to.contain("height: 4px");
       });
     });
-	});
+  });
 });
