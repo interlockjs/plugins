764c1bfe8193d6e023b356cd542b42f3b0896816
Dale Bustad <dale@divmain.com>
2016-09-22 22:44:44 -0700

    Disable `babel-plugin-minify-dead-code-elimination` until it stabilizes.

--- packages/babili/src/index.js
+++ packages/babili/src/index.js
@@ -2,8 +2,24 @@ import { transformFromAst } from "babel-core";
 import * as t from "babel-types";
 
 
-const babiliPreset = require.resolve("babel-preset-babili");
-
+const plugins = [
+  "babel-plugin-minify-constant-folding",
+  // This plugin does not yet work correctly with classes and ES6 exports.
+  // "babel-plugin-minify-dead-code-elimination",
+  "babel-plugin-minify-flip-comparisons",
+  "babel-plugin-minify-guarded-expressions",
+  "babel-plugin-minify-infinity",
+  "babel-plugin-minify-mangle-names",
+  "babel-plugin-minify-replace",
+  "babel-plugin-minify-simplify",
+  "babel-plugin-minify-type-constructors",
+  "babel-plugin-transform-member-expression-literals",
+  "babel-plugin-transform-merge-sibling-variables",
+  "babel-plugin-transform-minify-booleans",
+  "babel-plugin-transform-property-literals",
+  "babel-plugin-transform-simplify-comparison-operators",
+  "babel-plugin-transform-undefined-to-void"
+].map(require.resolve.bind(require));
 
 export default function () {
   return (override, transform) => {
@@ -11,7 +27,7 @@ export default function () {
       const config = {
         code: false,
         ast: true,
-        presets: [ babiliPreset ]
+        plugins
       };
 
       const programWrapper = t.program([ bundleAst ]);
